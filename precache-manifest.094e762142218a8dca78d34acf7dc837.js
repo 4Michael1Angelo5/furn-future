42cefabb0f0392bd07fb77e11e09098283fecfc3
Chris Chun <4michael1angelo5@gmail.com>
2019-11-24 14:02:40 -0800

    Updates

---
+++ precache-manifest.094e762142218a8dca78d34acf7dc837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266d98f7ca76fee60cd5b17047054283",
+    "url": "/furn-future/index.html"
+  },
+  {
+    "revision": "932795fa52166c2e4da3",
+    "url": "/furn-future/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "388c738e474f855d58e4",
+    "url": "/furn-future/static/js/2.29e11103.chunk.js"
+  },
+  {
+    "revision": "932795fa52166c2e4da3",
+    "url": "/furn-future/static/js/main.7f2d866f.chunk.js"
+  },
+  {
+    "revision": "14e35b62ee69d465d216",
+    "url": "/furn-future/static/js/runtime-main.467e271b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/furn-future/static/media/logo.25bf045c.svg"
+  }
+]);
