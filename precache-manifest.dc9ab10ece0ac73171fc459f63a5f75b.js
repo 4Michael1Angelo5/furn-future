6d068f243d94d06fa17924ee3c26ad2b762a7aaa
Chris Chun <4michael1angelo5@gmail.com>
2019-11-24 15:02:27 -0800

    Updates

---
+++ precache-manifest.dc9ab10ece0ac73171fc459f63a5f75b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fac295f31df17a5828015ecbbd06e3",
+    "url": "/furn-future/index.html"
+  },
+  {
+    "revision": "7421ae691066f969a03f",
+    "url": "/furn-future/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "94e42232772359d811b0",
+    "url": "/furn-future/static/css/main.28095c26.chunk.css"
+  },
+  {
+    "revision": "7421ae691066f969a03f",
+    "url": "/furn-future/static/js/2.3158c749.chunk.js"
+  },
+  {
+    "revision": "94e42232772359d811b0",
+    "url": "/furn-future/static/js/main.15e5a712.chunk.js"
+  },
+  {
+    "revision": "14e35b62ee69d465d216",
+    "url": "/furn-future/static/js/runtime-main.467e271b.js"
+  },
+  {
+    "revision": "ed1d014bf2b8b72f27d6af65c69f710a",
+    "url": "/furn-future/static/media/TitilliumWeb-Regular.ed1d014b.ttf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/furn-future/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e15c55267f79ae97899ba85de1fbeb2a",
+    "url": "/furn-future/static/media/tree.e15c5526.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/furn-future/precache-manifest.094e762142218a8dca78d34acf7dc837.js"
+  "/furn-future/precache-manifest.dc9ab10ece0ac73171fc459f63a5f75b.js"
 );
 
 self.addEventListener('message', (event) => {
